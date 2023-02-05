12b254c1732f5d93865bf891ec7d82bd97c5cde4
Quang Nguyen <qtn1113@gmail.com>
2023-02-05 10:50:59 -0600

    Yeehaw music looping

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+
+    [SerializeField] AudioSource IntroBG;
+    [SerializeField] AudioSource LoopBG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IntroBG.Play();
+        LoopBG.PlayDelayed(IntroBG.clip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
