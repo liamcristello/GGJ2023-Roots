5b9a62e0885a2e2f148ce9f0030f7df89b4540a7
Quang Nguyen <qtn1113@gmail.com>
2023-02-04 19:59:57 -0600

    Created Prefabs for a bunch of player and bomb flower components

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -13,9 +13,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveLimiter = 0.7f;
     [SerializeField] float runSpeed = 300.0f;
 
+    Animator playerAnimator;
+    SpriteRenderer playerSprite;
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -23,6 +28,19 @@ public class PlayerController : MonoBehaviour
         // Gives a value between -1 and 1
         horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
         vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            playerAnimator.SetFloat("XInput", horizontal);
+            playerAnimator.SetFloat("YInput", vertical);
+            playerAnimator.SetTrigger("Walking");
+        }
+        else
+        {
+            playerAnimator.SetTrigger("Idle");
+        }
+
+        CheckOrientation();
     }
 
     void FixedUpdate()
@@ -35,5 +53,20 @@ public class PlayerController : MonoBehaviour
         }
 
         body.velocity = new Vector2(horizontal * runSpeed * Time.deltaTime, vertical * runSpeed * Time.deltaTime);
+    }
+
+    void CheckOrientation()
+    {
+        if(horizontal != 0)
+        {
+            if (horizontal < 0)
+            {
+                playerSprite.flipX = true;
+            }
+            else
+            {
+                playerSprite.flipX = false;
+            }
+        }
     }
 }
